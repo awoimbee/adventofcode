5e96a1d1f972fb7c2eec72a3e922ae75f6945ca3
Arthur Woimbée <arthur.woimbee@gmail.com>
2020-12-18 17:39:52 +0100

    day16 done

--- src/day15.rs
+++ src/day15.rs
@@ -3,21 +3,26 @@ const INPUT: &str = include_str!("../input/day15.txt");
 const ITERATIONS_P1: usize = 2020;
 const ITERATIONS_P2: usize = 30000000;
 
-fn parse() -> (Vec<isize>, usize, usize) {
+fn parse() -> (Box<[isize; ITERATIONS_P2]>, usize, usize) {
     let numbers: Vec<_> = INPUT
         .trim()
         .split(',')
         .map(|x| x.parse().unwrap())
         .collect();
     let (&last_number, rest) = numbers.split_last().unwrap();
-    let mut number_timestamps = vec![-1; ITERATIONS_P2];
+    let mut number_timestamps = box [-1; ITERATIONS_P2];
     for (n, i) in rest.iter().enumerate() {
         number_timestamps[*i] = n as isize + 1;
     }
     (number_timestamps, numbers.len(), last_number)
 }
 
-fn run_until(round: &mut usize, nb_timestamps: &mut [isize], last_nb: &mut usize, max_iter: usize) {
+fn run_until(
+    round: &mut usize,
+    nb_timestamps: &mut Box<[isize; ITERATIONS_P2]>,
+    last_nb: &mut usize,
+    max_iter: usize,
+) {
     while *round < max_iter {
         let new_number = if nb_timestamps[*last_nb] != -1 {
             *round - nb_timestamps[*last_nb] as usize
--- src/day16.rs
+++ src/day16.rs
@@ -1,92 +1,160 @@
-use std::{cell::RefCell, collections::HashMap};
-
 const INPUT: &str = include_str!("../input/day16.txt");
 
-struct Parsed {
-    values: HashMap<&'static str, [[u64; 2]; 2]>,
-    my_ticket: Vec<u64>,
-    nearby_tickets: RefCell<Vec<Vec<u64>>>,
+fn p1_bis(
+    nearby_tickets: impl Iterator<Item = Vec<u64>>,
+    schema: &Vec<(&'static str, [[u64; 2]; 2])>,
+) -> (Vec<Vec<u64>>, u64) {
+    let mut invalid_sum = 0;
+    let valid_tickets = nearby_tickets
+        .filter(|ticket| {
+            ticket.iter().all(|val| {
+                let valid = schema.iter().any(|(_, ranges)| {
+                    ranges
+                        .iter()
+                        .any(|range| (range[0]..=range[1]).contains(val))
+                });
+                if !valid {
+                    invalid_sum += val;
+                }
+                valid
+            })
+        })
+        .collect();
+
+    (valid_tickets, invalid_sum)
 }
 
-impl Parsed {
-    pub fn new() -> Self {
-        let sep_your = INPUT.find("your ticket:").unwrap();
-        let sep_nearby = sep_your + INPUT[sep_your..].find("nearby tickets:").unwrap();
-        let values = INPUT[..sep_your]
-            .trim()
-            .lines()
-            .map(|l| {
-                let mut t = l.split(": ");
-                let name = t.next().unwrap();
-                let mut values = [[0; 2]; 2];
-                for (i, range) in t.next().unwrap().split(" or ").enumerate() {
-                    for (j, nb) in range.split('-').enumerate() {
-                        values[i][j] = nb.parse().unwrap();
-                    }
-                }
-                (name, values)
-            })
-            .collect();
-        let my_ticket = INPUT[sep_your + 13..sep_nearby]
-            .trim()
-            .split(',')
-            .map(|nb| nb.parse().unwrap())
-            .collect();
-        let nearby_tickets = RefCell::new(
-            INPUT[sep_nearby + 16..]
-                .trim()
-                .lines()
-                .map(|l| l.split(',').map(|nb| nb.parse().unwrap()).collect())
-                .collect(),
-        );
-        Self {
-            values,
-            my_ticket,
-            nearby_tickets,
+fn p2(schema: Vec<(&str, [[u64; 2]; 2])>, ticket: Vec<u64>, nearby: Vec<Vec<u64>>) -> u64 {
+    let indexes: Vec<_> = schema
+        .iter()
+        .map(|(name, _)| name)
+        .enumerate()
+        .filter(|(_, name)| name.starts_with("departure"))
+        .map(|(index, _)| index)
+        .collect();
+
+    let confusion_matrix = p2_generate_confusion_matrix(&nearby, &schema);
+    let matching = p2_solve(&confusion_matrix);
+
+    let ans: u64 = indexes
+        .into_iter()
+        .map(|i| ticket[matching.iter().position(|&x| x == i).unwrap()])
+        .product();
+    ans
+}
+
+fn p2_generate_confusion_matrix(
+    nearby: &Vec<Vec<u64>>,
+    schema: &Vec<(&str, [[u64; 2]; 2])>,
+) -> Vec<Vec<bool>> {
+    let columns = p2_transpose(&nearby);
+    let m = schema.len();
+    let mut ans = vec![vec![false; m]; m];
+    for i in 0..m {
+        for j in 0..m {
+            if p2_check(&columns[i], &schema[j].1) {
+                ans[i][j] = true;
+            }
         }
     }
+    return ans;
 }
 
-fn p1(input: &mut Parsed) -> u64 {
-    let mut invalid_sum = 0;
-    input.nearby_tickets.borrow_mut().retain(|ticket| {
-        let ticket_invalid_sum = ticket
+fn p2_transpose(nearby: &Vec<Vec<u64>>) -> Vec<Vec<u64>> {
+    let m = nearby[0].len();
+
+    let mut ans = vec![Vec::new(); m];
+    for numbers in nearby {
+        for (i, &number) in numbers.iter().enumerate() {
+            ans[i].push(number);
+        }
+    }
+    return ans;
+}
+
+fn p2_check(numbers: &[u64], intervals: &[[u64; 2]]) -> bool {
+    numbers.iter().all(|num| {
+        intervals
             .iter()
-            .map(|val| {
-                let mut sum = 0;
-                let valid = input
-                    .values
-                    .iter()
-                    .map(|(_, v)| {
-                        for r in v {
-                            if (r[0]..=r[1]).contains(val) {
-                                return true;
-                            };
-                        }
-                        false
-                    })
-                    .any(|x| x);
-                if !valid {
-                    sum += *val;
-                }
-                sum
-            })
-            .sum::<u64>();
-        invalid_sum += ticket_invalid_sum;
-        ticket_invalid_sum == 0
-    });
-    invalid_sum
+            .any(|range| range[0] <= *num && *num <= range[1])
+    })
 }
 
-fn p2(input: &Parsed) -> u64 {
-    0
+fn p2_solve(valid: &Vec<Vec<bool>>) -> Vec<usize> {
+    let mut valid = valid.clone();
+
+    let mut ans = vec![999_999; valid.len()];
+    for _ in 0..valid.len() {
+        let (i, v) = valid
+            .iter()
+            .enumerate()
+            .min_by_key(|&(_, item)| {
+                let v = item.iter().filter(|x| **x).count();
+                if v == 0 {
+                    return 999_999;
+                }
+                return v;
+            })
+            .unwrap();
+        let indexes: Vec<_> = v
+            .iter()
+            .enumerate()
+            .filter(|(_, x)| **x)
+            .map(|(i, _)| i)
+            .collect();
+        if indexes.len() > 1 {
+            println!("This is very bad: {:?}", indexes);
+            panic!();
+        }
+        for i in 0..valid.len() {
+            valid[i][indexes[0]] = false;
+        }
+        ans[i] = indexes[0];
+    }
+
+    return ans;
 }
 
 pub fn day16() -> (String, String) {
-    let mut parsed = Parsed::new();
+    let mut sections = INPUT.trim().split("\n\n");
 
-    let p1 = p1(&mut parsed);
-    let p2 = p2(&parsed);
+    let schema: Vec<_> = sections
+        .next()
+        .unwrap()
+        .lines()
+        .map(|l| {
+            let mut t = l.split(": ");
+            let name = t.next().unwrap();
+            let mut values = [[0; 2]; 2];
+            for (i, range) in t.next().unwrap().split(" or ").enumerate() {
+                for (j, nb) in range.split('-').enumerate() {
+                    values[i][j] = nb.parse::<u64>().unwrap();
+                }
+            }
+            (name, values)
+        })
+        .collect();
+
+    let my_ticket: Vec<u64> = sections
+        .next()
+        .unwrap()
+        .lines()
+        .skip(1)
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|nb| nb.parse().unwrap())
+        .collect();
+
+    let nearby_tickets = sections
+        .next()
+        .unwrap()
+        .lines()
+        .skip(1)
+        .map(|l| l.split(',').map(|nb| nb.parse().unwrap()).collect());
+
+    let (nearby_tickets, p1) = p1_bis(nearby_tickets, &schema);
+    let p2 = p2(schema, my_ticket, nearby_tickets);
 
     (format!("{}", p1), format!("{}", p2))
 }

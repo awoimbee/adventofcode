872a38e962e3bd8bbc3c90bc3e457be1ae740eaa
Arthur Woimbée <arthur.woimbee@gmail.com>
2023-12-06 16:30:52 +0100

    day06 done

---
+++ 2023/src/day06.rs
@@ -0,0 +1,63 @@
+const INPUT: &str = include_str!("../input/day06.txt");
+
+fn ways_to_win_race(time: i64, distance: i64) -> i64 {
+    let mut ways_to_win = 0;
+    for button_time in 0..time {
+        // To win: speed * remaining_time > distance
+        // speed = buttom_time, remaining_time = time - button_time
+        // => button_time * (time - button_time) > distance
+        if button_time * (time - button_time) > distance {
+            ways_to_win += 1;
+        }
+    }
+    ways_to_win
+}
+
+fn solve_p1(input: &str) -> i64 {
+    let mut lines = input.lines();
+    let times: Vec<_> = lines.next().unwrap().split_whitespace().skip(1).map(|num| num.parse().unwrap()).collect();
+    let distances: Vec<_> = lines.next().unwrap().split_whitespace().skip(1).map(|num| num.parse().unwrap()).collect();
+    let mut p1 = 1;
+
+    for (time, distance) in times.into_iter().zip(distances.into_iter()) {
+        p1 *= ways_to_win_race(time, distance);
+    }
+
+    p1
+}
+
+fn solve_p2(input: &str) -> i64 {
+    let mut lines = input.lines();
+    let time = lines.next().unwrap().split_whitespace().skip(1).collect::<String>().parse().unwrap();
+    let distance = lines.next().unwrap().split_whitespace().skip(1).collect::<String>().parse().unwrap();
+
+    ways_to_win_race(time, distance)
+}
+
+pub fn day06() -> (String, String) {
+    let p1 = solve_p1(INPUT);
+    let p2 = solve_p2(INPUT);
+
+    (p1.to_string(), p2.to_string())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use indoc::indoc;
+
+    const TEST_INPUT: &str = indoc! {r#"
+        Time:      7  15   30
+        Distance:  9  40  200
+    "#};
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(solve_p1(TEST_INPUT), 288);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(solve_p2(TEST_INPUT), 71503);
+    }
+}
--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -3,7 +3,7 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
-// mod day06;
+mod day06;
 // mod day07;
 // mod day08;
 // mod day09;
@@ -28,13 +28,13 @@ use clap::Parser;
 use colored::*;
 use std::time::SystemTime;
 
-const SOLUTIONS: [Day; 5] = [
+const SOLUTIONS: [Day; 6] = [
     day01::day01,
     day02::day02,
     day03::day03,
     day04::day04,
     day05::day05,
-    // day06::day06,
+    day06::day06,
     // day07::day07,
     // day08::day08,
     // day09::day09,

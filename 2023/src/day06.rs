199c0a58b973a9b4efbe1fe0115582204b68d2ba
Arthur Woimbée <arthur.woimbee@gmail.com>
2023-12-08 00:28:29 +0100

    optimize day06

--- 2023/src/day06.rs
+++ 2023/src/day06.rs
@@ -1,6 +1,7 @@
 const INPUT: &str = include_str!("../input/day06.txt");
 
-fn ways_to_win_race(time: i64, distance: i64) -> i64 {
+/// In part one, time is small but there are many races.
+fn ways_to_win_race_p1(time: i64, distance: i64) -> i64 {
     let mut ways_to_win = 0;
     for button_time in 0..time {
         // To win: speed * remaining_time > distance
@@ -8,9 +9,27 @@ fn ways_to_win_race(time: i64, distance: i64) -> i64 {
         // => button_time * (time - button_time) > distance
         if button_time * (time - button_time) > distance {
             ways_to_win += 1;
+        } else if ways_to_win > 0 {
+            // This is a quadratic function, once solutions stop coming in we can early exit!
+            break;
         }
     }
     ways_to_win
+}
+
+/// For part 2 we have a single race with a large time.
+/// In this case doing math is faster !
+fn ways_to_win_race_p2(time: i64, distance: i64) -> i64 {
+    // Bt * T - Bt > D
+    // <=> -1 * (Bt^2) + T * Bt - D > 0
+    // => Quadratic equation, solve for `= 0` then calculate the interval
+    //   -> We know Bt can't be +inf or -inf so the sol in between the bounds
+    let d = ((time.pow(2) - 4 * distance) as f64).sqrt();
+    let bounds = (
+        ((time as f64 + d) / 2.).floor() as i64,
+        ((time as f64 - d) / 2.).ceil() as i64,
+    );
+    bounds.0 - bounds.1 + 1
 }
 
 fn solve_p1(input: &str) -> i64 {
@@ -32,7 +51,7 @@ fn solve_p1(input: &str) -> i64 {
     let mut p1 = 1;
 
     for (time, distance) in times.into_iter().zip(distances.into_iter()) {
-        p1 *= ways_to_win_race(time, distance);
+        p1 *= ways_to_win_race_p1(time, distance);
     }
 
     p1
@@ -57,7 +76,7 @@ fn solve_p2(input: &str) -> i64 {
         .parse()
         .unwrap();
 
-    ways_to_win_race(time, distance)
+    ways_to_win_race_p2(time, distance)
 }
 
 pub fn day06() -> (String, String) {
